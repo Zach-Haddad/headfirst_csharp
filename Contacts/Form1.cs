a7b156d2974fc06c4c8f0a47dcb22b9aee21c993
Zach Haddad <zhaddad7@gmail.com>
2017-06-12 16:50:15 -0700

    Set up SQL Db, basic form, publish

--- Contacts/Form1.cs
+++ Contacts/Form1.cs
@@ -19,12 +19,27 @@ namespace Contacts
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            // TODO: This line of code loads data into the 'contactsDataSet.People' table. You can move, or remove it, as needed.
+            this.peopleTableAdapter.Fill(this.contactsDataSet.People);
 
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Contact list written by Zach Haddad", "About");
+        }
+
+        private void peopleBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.peopleBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.contactsDataSet);
+
+        }
+
+        private void client_CheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
